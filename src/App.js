a97ddd6f0ebfa8b6b2f8badaeeea0c3d6f8d341f
Kogilathota Vijay Reddy <vijay.kogilathota@peopletech.com>
2023-10-17 11:56:52 +0530

    delete files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 import './App.css';
 
-import Navbar from "./components/Navbar/Navbar";
+
 import Home from './components/Home/Home';
-import About from './components/About/About';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <Home />
-      <About />
+      
     </div>
   );
 }
--- src/components/Global/Container.js
+++
@@ -1,9 +0,0 @@
-
-import Cont from './Cont.css';
-
-const Container = () => (
-    <div className="container" Cont={Cont}></div>
-)
-
-
-export default Container;
